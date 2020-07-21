14d0e6248f17be20d034d5670a227e171eab957b
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-21 23:32:21 +0530

    Error Boundary

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React , {Component} from 'react';
+
+class ErrorBoundary extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            hasError : false
+        }
+    }
+
+componentDidCatch(error , info){
+    this.setState({hasError:true})
+}
+
+    render(){
+        if (this.state.hasError){
+            return <h1>Oooooops. That is not good.</h1>
+        }
+        else{
+            return this.props.children
+        }
+    }
+}
+
+export default ErrorBoundary;
